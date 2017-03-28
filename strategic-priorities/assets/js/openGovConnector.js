f08138d5468b14323db50d97ce9e6eb71201f392
Sami Baig <samibaig@gmail.com>
2017-03-28 15:51:44 -0400

    Strategic Priotities

---
+++ strategic-priorities/assets/js/openGovConnector.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+   
+    $(".search-query-btn").click(function () {
+        var tempParams = $('.search-query').val();
+        sendToOG(tempParams);
+    });
+
+    $('.search-query').keydown(function (e) {
+        if (e.keyCode == 13) {
+            var tempParams = $('.search-query').val();
+            sendToOG(tempParams);
+        }
+    })
+});
+
+function sendToOG(in_search_params) {
+    var ogUri = "http://tempesandbox.ogopendata.com/dataset?q=";
+    var encodedVal = encodeURIComponent(in_search_params);
+    window.location = ogUri + encodedVal;
+    return false;
+}
